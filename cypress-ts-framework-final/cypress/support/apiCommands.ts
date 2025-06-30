a5720bc6fcd0bf7907e2f637256c227d56fd5d8f
Nikhil Kumar <106591481+ErNikhil11@users.noreply.github.com>
2025-06-30 20:25:36 +0530

    Delete cypress-ts-framework-final directory

--- cypress-ts-framework-final/cypress/support/apiCommands.ts
+++
@@ -1,5 +0,0 @@
-Cypress.Commands.add('getUserById', (id) => {
-  cy.request(`/api/users/${id}`).then((response) => {
-    expect(response.status).to.eq(200)
-  })
-})
--- cypress-ts-framework-final/cypress/support/commands.ts
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('login', (username: string, password: string) => {
-  cy.visit('/login')
-  cy.get('#username').type(username)
-  cy.get('#password').type(password)
-  cy.get('form').submit()
-})
